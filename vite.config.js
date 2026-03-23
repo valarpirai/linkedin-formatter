4dde00bd0baa67868e5684a4796096db75de967e
Valar <a.valarpirai@gmail.com>
2026-03-23 12:01:05 +0530

    Rename to docs

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/formatter/'
+  base: '/linkedin-formatter/',
+  build: {
+    outDir: 'docs'
+  }
 })
